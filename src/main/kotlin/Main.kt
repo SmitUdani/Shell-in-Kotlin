af3b59fc9aed62443faad6cfd6e64cf99a21ea51
SmitUdani <smitudani04@gmail.com>
2025-06-08 16:25:48 +0530

    [Type]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,19 @@ fun main() {
         val command = readln()
         when {
             command.startsWith("echo") -> println(command.substringAfter("echo "))
+            command.startsWith("type") -> handleTypeCommand(command)
             command == "exit 0" -> break
             else -> println("$command: command not found")
         }
     }
+}
+
+fun handleTypeCommand(command: String) {
+    val subCommand = command.substringAfter("type ")
+    val builtInCommands = setOf("type", "echo", "exit")
+
+    if(subCommand !in builtInCommands)
+        println("$subCommand: not found")
+
+    else println("$subCommand is a shell builtin")
 }
